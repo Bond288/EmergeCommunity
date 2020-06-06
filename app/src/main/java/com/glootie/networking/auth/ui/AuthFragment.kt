8c412ace43aefc5f6437f82dfd17a1198b8bc284
Valentin Makarov <alter23punk@gmail.com>
2020-06-06 21:49:50 +0700

    Добавил вход через фейсбук, переписал все на фрагменты

--- app/src/main/java/com/glootie/networking/auth/ui/AuthFragment.kt
+++ app/src/main/java/com/glootie/networking/auth/ui/AuthFragment.kt
@@ -1,5 +1,6 @@
 package com.glootie.networking.auth.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -9,18 +10,26 @@ import android.view.ViewGroup
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
+import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.glootie.networking.R
 import com.glootie.networking.auth.presentation.AuthViewModel
 import com.glootie.networking.base.BaseFragment
+import com.glootie.networking.qr.TicketScannerFragment
+import com.glootie.networking.startup.ui.StartupListFragment
 import kotlinx.android.synthetic.main.fragment_auth.*
+import java.util.*
 
 class AuthFragment : BaseFragment() {
 
     companion object {
 
+        const val SCAN_TICKET_RESULT_CODE = 101
+        const val FACEBOOK_LOGIN_RESULT_CODE = 64206
         const val PERMISSION = "email"
     }
+
+    private var isLogin = false
 
     private lateinit var viewModel: AuthViewModel
     private val callbackManager = CallbackManager.Factory.create()
@@ -35,14 +44,17 @@ class AuthFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = getViewModel()
         initFacebookAuthButton()
+        initScannerButton()
     }
 
 
     private fun initFacebookAuthButton() {
-        facebook_login_button.setPermissions(arrayListOf(PERMISSION))
-        facebook_login_button.fragment = this
-        facebook_login_button.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+        facebook_login.setOnClickListener { loginFacebook() }
+    }
 
+    private fun loginFacebook() {
+        LoginManager.getInstance().logInWithReadPermissions(this, arrayListOf(PERMISSION));
+        LoginManager.getInstance().registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
             override fun onSuccess(result: LoginResult?) {
                 result ?: return
             }
@@ -55,8 +67,31 @@ class AuthFragment : BaseFragment() {
         })
     }
 
+    private fun initScannerButton() {
+        scanner.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val fragment = TicketScannerFragment()
+            fragmentTransaction.add(R.id.fragment_container_view_tag, fragment)
+            fragmentTransaction.commit()
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager.onActivityResult(requestCode, resultCode, data)
+        isLogin = when (requestCode) {
+            SCAN_TICKET_RESULT_CODE -> resultCode == Activity.RESULT_OK
+            FACEBOOK_LOGIN_RESULT_CODE -> resultCode == Activity.RESULT_OK
+            else -> false
+        }
+
+        if (isLogin) {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val fragment = StartupListFragment()
+            fragmentTransaction.add(R.id.fragment_container_view_tag, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
---
+++ app/src/main/java/com/glootie/networking/qr/TicketScannerFragment.kt
@@ -0,0 +1,144 @@
+package com.glootie.networking.qr
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.util.SparseArray
+import android.view.*
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.glootie.networking.R
+import com.glootie.networking.base.BaseFragment
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import kotlinx.android.synthetic.main.fragment_ticket_scanner.*
+import java.io.IOException
+import javax.security.auth.callback.Callback
+
+
+class TicketScannerFragment : BaseFragment() {
+
+    var surfaceView: SurfaceView? = null
+    private var barcodeDetector: BarcodeDetector? = null
+    private var cameraSource: CameraSource? = null
+    private val REQUEST_CAMERA_PERMISSION = 201
+    var btnAction: Button? = null
+    var intentData = ""
+    var isUrl = false
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_ticket_scanner, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        txtBarcodeValue.setOnClickListener {
+            if (intentData.isNotEmpty()) {
+                if (isUrl)
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(intentData)))
+                else {
+                    Toast.makeText(activity, getString(R.string.target_camera_to_ticket), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun initialiseDetectorsAndSources() {
+        Toast.makeText(activity, getString(R.string.ticket_scanner_started), Toast.LENGTH_SHORT)
+            .show()
+        barcodeDetector = BarcodeDetector.Builder(activity)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+        cameraSource = CameraSource.Builder(activity, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+        surfaceView?.holder?.addCallback(object : Callback, SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder?) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(
+                            activity!!,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        cameraSource!!.start(surfaceView?.holder)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            activity!!,
+                            arrayOf(Manifest.permission.CAMERA),
+                            REQUEST_CAMERA_PERMISSION
+                        )
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder?,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder?) {
+                cameraSource!!.stop()
+            }
+        })
+        barcodeDetector?.run {
+            setProcessor(object : Detector.Processor<Barcode?> {
+                override fun release() {
+                    Toast.makeText(
+                        activity!!.applicationContext,
+                        "To prevent memory leaks barcode scanner has been stopped",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun receiveDetections(detections: Detector.Detections<Barcode?>) {
+                    val barcodes: SparseArray<Barcode?>? = detections.getDetectedItems()
+                    if (barcodes?.size() != 0) {
+                        txtBarcodeValue!!.post {
+                            if (barcodes?.valueAt(0)?.url != null) {
+                                isUrl = true
+                                btnAction!!.text = getString(R.string.login_to_conference)
+                                intentData = barcodes.valueAt(0)?.displayValue.toString()
+                                txtBarcodeValue!!.text = intentData
+                            } else {
+                                isUrl = false
+                                btnAction!!.text = getString(R.string.ticket_not_recognize)
+                                intentData = barcodes?.valueAt(0)?.displayValue.toString()
+                                txtBarcodeValue!!.text = intentData
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        cameraSource?.release()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initialiseDetectorsAndSources()
+    }
+}
